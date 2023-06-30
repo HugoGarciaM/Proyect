f5cb47b49c27b6047cd874b0cab8264bb7a50ff7
PEÑITA <cristianpenafielcastillo@gmail.com>
2023-06-30 04:27:33 -0400

    "PRUEBA BASE DE DATOS LOGGIN"

--- src/main/java/view/Login.java
+++ src/main/java/view/Login.java
@@ -17,8 +17,8 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
-        //LoadImage logo=new LoadImage ("/img/LOGO.jpg",jLabel4.getHeight(),jLabel4.getWidth());
-        //jLabel4.setIcon(logo.getImg());
+        LoadImage logo=new LoadImage ("/img/LOGO.jpg",jLabel4.getHeight(),jLabel4.getWidth());
+        jLabel4.setIcon(logo.getImg());
         new loginController(this);
         
     }
