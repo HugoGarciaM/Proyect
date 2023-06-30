7f995b9acb638ee527d14233a0190362166601a6
Andy <hg750792@gmail.com>
2023-06-30 13:07:43 -0400

    Login Terminado

--- src/main/java/view/Login.java
+++ src/main/java/view/Login.java
@@ -19,12 +19,13 @@ public class Login extends javax.swing.JFrame {
      */
     public Login() {
         initComponents();
-        LoadImage logo=new LoadImage ("/img/llave.png",jLabel4.getHeight(),jLabel4.getWidth());
-        jLabel4.setIcon(logo.getImg());
+        LoadImage logo=new LoadImage ("/img/llave.png",lblOpacar.getHeight(),lblOpacar.getWidth());
+        lblOpacar.setIcon(logo.getImg());
         LoadImage logo1=new LoadImage ("/img/logoemp.jpg",lblLogin.getHeight(),lblLogin.getWidth());
         lblLogin.setIcon(logo1.getImg());
         LoadImage logo2 =new LoadImage ("/img/Fondo.jpg",lblFondo.getHeight(),lblFondo.getWidth());
         lblFondo.setIcon(logo2.getImg());
+        lblOpacar.setBackground(new java.awt.Color(0,0,0,1));
         new loginController(this);
         
     }
@@ -39,7 +40,7 @@ public class Login extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel4 = new javax.swing.JLabel();
+        lblOpacar = new javax.swing.JLabel();
         lblContraseña = new javax.swing.JLabel();
         lblUsuario = new javax.swing.JLabel();
         txtUsuario = new javax.swing.JTextField();
@@ -48,37 +49,56 @@ public class Login extends javax.swing.JFrame {
         lblLogin = new javax.swing.JLabel();
         btnIngresar = new javax.swing.JButton();
         lblFondo = new javax.swing.JLabel();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(490, 90, 180, 140));
+        jPanel1.add(lblOpacar, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 80, 180, 140));
 
+        lblContraseña.setFont(new java.awt.Font("Stencil", 1, 18)); // NOI18N
         lblContraseña.setText("Contraseña:");
-        jPanel1.add(lblContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 110, -1, -1));
+        jPanel1.add(lblContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 310, 140, 20));
 
+        lblUsuario.setFont(new java.awt.Font("Stencil", 1, 18)); // NOI18N
         lblUsuario.setText("Usuario:");
         lblUsuario.setToolTipText("Usuario:");
-        jPanel1.add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 180, -1, -1));
-        jPanel1.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 110, -1, -1));
+        jPanel1.add(lblUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 250, -1, 30));
+
+        txtUsuario.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtUsuarioActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtUsuario, new org.netbeans.lib.awtextra.AbsoluteConstraints(160, 250, 110, -1));
 
         lblBienvenido.setFont(new java.awt.Font("Showcard Gothic", 1, 36)); // NOI18N
-        lblBienvenido.setForeground(new java.awt.Color(82, 200, 217));
+        lblBienvenido.setForeground(new java.awt.Color(51, 51, 51));
         lblBienvenido.setText("BIENVENIDO");
-        jPanel1.add(lblBienvenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 20, -1, -1));
+        jPanel1.add(lblBienvenido, new org.netbeans.lib.awtextra.AbsoluteConstraints(270, 40, -1, -1));
 
         txtContraseña.setText("jPasswordField1");
-        jPanel1.add(txtContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(140, 180, -1, -1));
-        jPanel1.add(lblLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 100, 110, 100));
+        txtContraseña.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtContraseñaActionPerformed(evt);
+            }
+        });
+        jPanel1.add(txtContraseña, new org.netbeans.lib.awtextra.AbsoluteConstraints(190, 310, -1, -1));
+        jPanel1.add(lblLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 30, 130, 120));
 
+        btnIngresar.setFont(new java.awt.Font("Monospac821 BT", 0, 24)); // NOI18N
         btnIngresar.setText("Ingresar");
         btnIngresar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnIngresarActionPerformed(evt);
             }
         });
-        jPanel1.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 350, -1, -1));
+        jPanel1.add(btnIngresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 370, -1, -1));
         jPanel1.add(lblFondo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 730, 430));
+
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel1.setText("jLabel1");
+        jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(558, 66, 170, 110));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -97,6 +117,14 @@ public class Login extends javax.swing.JFrame {
     private void btnIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngresarActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btnIngresarActionPerformed
+
+    private void txtUsuarioActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtUsuarioActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtUsuarioActionPerformed
+
+    private void txtContraseñaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtContraseñaActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtContraseñaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -125,7 +153,7 @@ public class Login extends javax.swing.JFrame {
         }
         //</editor-fold>
 try{
-            //FlatArcOrangeIJTheme.setup();
+            FlatArcOrangeIJTheme.setup();
             
         }catch(Exception e){
             JOptionPane.showMessageDialog(null, e);
@@ -140,12 +168,13 @@ try{
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     public javax.swing.JButton btnIngresar;
-    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JLabel lblBienvenido;
     private javax.swing.JLabel lblContraseña;
     private javax.swing.JLabel lblFondo;
     public javax.swing.JLabel lblLogin;
+    private javax.swing.JLabel lblOpacar;
     private javax.swing.JLabel lblUsuario;
     public javax.swing.JPasswordField txtContraseña;
     public javax.swing.JTextField txtUsuario;
